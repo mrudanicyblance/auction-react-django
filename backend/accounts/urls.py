86f644080ae2cb2e7d0e7bfbfcfbc815161e4420
Urmish Solanki <131744795+urmishsolanki2225@users.noreply.github.com>
2024-05-30 18:28:56 +0530

    Delete backend directory

--- backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views as v
-
-# url defined for accounts and user based functionality.
-urlpatterns = [
-    path('',v.loginaccounts,name="login_account"),
-    path('sign-up/',v.signupaccounts,name='signup_account'),
-    path('logout/',v.logout_view,name='logout_account')
-]
