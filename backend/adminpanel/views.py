9c9f7dea84bef48fbbec74dd9ae58d5bff6d2942
urmish <urmish.cyblance@gmail.com>
2024-06-03 11:37:52 +0530

    By Urmish Solanki

--- backend/adminpanel/views.py
+++ backend/adminpanel/views.py
@@ -94,17 +94,7 @@ def create_users(request):
         form = SignUpForm()
     return render(request, 'adminpanel/users/create.html', {'form': form})
 def edit_users(request):
-    #if request.method == 'POST':
-    #    user_form = UserUpdateForm(request.POST, instance=request.user)
-     #   profile_form = UserProfileForm(request.POST, instance=request.user.profile)
-     #   if user_form.is_valid() and profile_form.is_valid():
-      #      user_form.save()
-     ##       profile_form.save()
-      #      return redirect('profile')
-   # else:
-     #   user_form = UserUpdateForm(instance=request.user)
-     #   profile_form = UserProfileForm(instance=request.user.profile)
-     return render(request, 'adminpanel/users/edit.html', {
-            #'user_form': user_form,
-           # 'profile_form': profile_form
-        })
+    return render(request, 'adminpanel/users/edit.html', {
+        #'user_form': user_form,
+        #'profile_form': profile_form
+    })
