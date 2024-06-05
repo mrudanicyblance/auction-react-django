9285bf10c2545f69c46ec7742b9b42591977ae7f
Mrudu30 <mrudanisongade@gmail.com>
2024-06-05 09:48:37 +0530

    field_views changed

--- backend/static/js/fields_view.js
+++ backend/static/js/fields_view.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('#addFields').submit(function(e){
         e.preventDefault();
         var field_id = $('#fieldId').val();
-        var url = field_id ? 'http://127.0.0.1:8000/edit_field/' : 'http://127.0.0.1:8000/add_field/';
+        var url = field_id ? 'http://127.0.0.1:8000/field/edit/' : 'http://127.0.0.1:8000/field/add/';
         var form_type = field_id ? 'updated' : 'created';
         $.ajax({
             type: "post",
@@ -45,6 +45,34 @@ function update_field(button){
 
     $('#addFieldsModal').modal('show');
 }
-function delete_fields(button){
-    
+
+function delete_field(button){
+    var fieldId = $(button).data('field-id');
+    var result = confirm('Are you sure you want to delete this field?')
+    if (result){
+        $.ajax({
+            type: "post",
+            url: "https://127.0.0.1:8000/field/delete/",
+            data: {'fieldId':fieldId},
+            success: function (response) {
+                if (response.status == 'success'){
+                    // alert('added')
+                    $('#general_messages').show()
+                    $('#general_messages').addClass('alert alert-success').text(`Field ${response.field_name} has been deleted!`)
+                    setTimeout(() => {
+                        $('#general_messages').hide()
+                        $('#general_messages').removeClass('alert alert-success').text(``)
+                    }, 3000);
+                }else{
+                    // alert('error')
+                    $('#general_messages').show()
+                    $('#general_messages').addClass('alert alert-danger').text(`Some error occured`)
+                    setTimeout(() => {
+                        $('#general_messages').hide()
+                        $('#general_messages').removeClass('alert alert-danger').text(``)
+                    }, 3000);
+                }
+            }
+        });
+    }
 }
